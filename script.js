7866e1a664443d9148741d747109e268783bd6c8
Christopher Wilson <cmw.wilson88@gmail.com>
2016-12-15 17:30:44 -0500

    Second Video.  Add randomizer to picking correct color

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var colors = [
-"rgb(255,0,0)",
+"rgb(255, 0, 0)",
 "rgb(255, 255, 0)",
 "rgb(0, 255, 0)",
 "rgb(0, 255, 255)",
@@ -8,8 +8,9 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
-var colorDisplay = document.getElementById("colorDisplay")
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -22,9 +23,27 @@ for (var i =0; i< squares.length; i++) {
 		var clickedColor = this.style.background
 		//compare color to pickedColor
 		if (clickedColor === pickedColor) {
-			alert("Correct");
+			messageDisplay.textContent = "Correct!";
+			changeColors(clickedColor);
 		} else {
-			alert("Wrong");
+			this.style.background = "#232323";
+			messageDisplay.textContent = "Try Again"
+
 		}
 	});
+}
+
+function changeColors(color) {
+	//loop through all squares
+	for (var i = 0; i < squares.length; i++) {
+		//change each color to match given color
+		squares[i].style.background = color;
+	}
+}
+
+function pickColor(){
+	//pick a random number
+	var random = Math.floor(Math.random()*colors.length);
+	//Asssign random index to colors 
+	return colors[random];
 }
